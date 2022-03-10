4897ad4f064d54ecd1927e2e5b7086b2ce1afa45
John Murret <john.murret@hashicorp.com>
2022-03-10 12:28:42 -0700

    adding missing comma

--- control-plane/subcommand/server-acl-init/command_test.go
+++ control-plane/subcommand/server-acl-init/command_test.go
@@ -2561,7 +2561,7 @@ func TestRun_ValidateLoginToken_SecondaryDatacenter(t *testing.T) {
 			Roles:            []string{resourcePrefix + "-client-acl-role-dc2"},
 			GlobalAuthMethod: false,
 			GlobalToken:      false,
-		}
+		},
 	}
 	for _, c := range cases {
 		t.Run(c.ComponentName, func(t *testing.T) {
