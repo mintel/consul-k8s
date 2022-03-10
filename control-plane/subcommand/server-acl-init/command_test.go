8576723dfc5c4c4516cdcdce3af506d39d10b1ef
John Murret <john.murret@hashicorp.com>
2022-03-10 06:36:32 -0700

    Rename -create-client-token flag to -client

--- control-plane/subcommand/server-acl-init/command_test.go
+++ control-plane/subcommand/server-acl-init/command_test.go
@@ -164,7 +164,7 @@ func TestRun_TokensPrimaryDC(t *testing.T) {
 	}{
 		{
 			TestName:    "Client token",
-			TokenFlags:  []string{"-create-client-token"},
+			TokenFlags:  []string{"-client"},
 			PolicyNames: []string{"client-token"},
 			PolicyDCs:   []string{"dc1"},
 			SecretNames: []string{resourcePrefix + "-client-acl-token"},
@@ -366,7 +366,7 @@ func TestRun_TokensReplicatedDC(t *testing.T) {
 	}{
 		{
 			TestName:    "Client token",
-			TokenFlags:  []string{"-create-client-token"},
+			TokenFlags:  []string{"-client"},
 			PolicyNames: []string{"client-token-dc2"},
 			PolicyDCs:   []string{"dc2"},
 			SecretNames: []string{resourcePrefix + "-client-acl-token"},
@@ -482,7 +482,7 @@ func TestRun_TokensWithProvidedBootstrapToken(t *testing.T) {
 	}{
 		{
 			TestName:    "Client token",
-			TokenFlags:  []string{"-create-client-token"},
+			TokenFlags:  []string{"-client"},
 			PolicyNames: []string{"client-token"},
 			SecretNames: []string{resourcePrefix + "-client-acl-token"},
 		},
@@ -1795,7 +1795,7 @@ func TestRun_SkipBootstrapping_WhenServersAreDisabled(t *testing.T) {
 		"-server-port=" + serverURL.Port(),
 		"-bootstrap-token-file=" + tokenFile,
 		"-set-server-tokens=false",
-		"-create-client-token=false", // disable client token, so there are fewer calls
+		"-client=false", // disable client token, so there are fewer calls
 	})
 	require.Equal(t, 0, responseCode, ui.ErrorWriter.String())
 
@@ -1906,7 +1906,7 @@ func TestRun_ACLReplicationTokenValid(t *testing.T) {
 		"-server-port", strings.Split(secondaryAddr, ":")[1],
 		"-resource-prefix=" + resourcePrefix,
 		"-acl-replication-token-file", tokenFile,
-		"-create-client-token",
+		"-client",
 		"-create-mesh-gateway-token",
 	}
 	responseCode := secondaryCmd.Run(secondaryCmdArgs)
